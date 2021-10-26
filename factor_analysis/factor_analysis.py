4526282f7c2aea2ee9f749cc7be2eb734b7d9c2c
Moritz Mistol <moritz.mistol@gmail.com>
2021-10-26 13:14:50 +0200

    add factor_analysis.py stub

---
+++ factor_analysis/factor_analysis.py
@@ -0,0 +1,10 @@
+"""
+Exploratory Factor Analysis
+"""
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class FactorAnalysis(BaseEstimator, TransformerMixin):
+    pass
