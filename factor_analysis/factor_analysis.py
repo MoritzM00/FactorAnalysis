794abdacba259821387b0bace7903941a0613c65
Moritz Mistol <moritz.mistol@gmail.com>
2021-11-04 15:25:24 +0100

    add stub methods with docstrings

--- factor_analysis/factor_analysis.py
+++ factor_analysis/factor_analysis.py
@@ -1,9 +1,55 @@
 """
-Exploratory Factor Analysis
+Exploratory Factor Analysis.
 """
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class FactorAnalysis(BaseEstimator, TransformerMixin):
-    pass
+    """
+    The linear factor analysis model.
+
+    TODO: docstring
+
+    Parameters
+    ----------
+    n_factors : int
+        The number of factors.
+    """
+
+    def __init__(self, n_factors):
+        self.n_factors = n_factors
+
+    def fit(self, X, y=None):
+        """
+        Fits the factor analysis model to the data.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+            Training data.
+        y : ignored
+            Not used, only for API consistency
+
+        Returns
+        -------
+        self : FactorAnalysis
+            The fitted model.
+        """
+        pass
+
+    def transform(self, X):
+        """
+        Transforms the data using the factors.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+            Samples.
+
+        Returns
+        -------
+        X_new : array_like, shape (n_samples, n_factors)
+            The transformed samples.
+        """
+        pass
