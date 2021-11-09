37089ee336c2e08258efcb85628e700576396441
Moritz Mistol <moritz.mistol@gmail.com>
2021-11-09 20:58:34 +0100

    move example from .ipynb to .py

---
+++ examples/plot_big_five_personalities.py
@@ -0,0 +1,32 @@
+"""
+========================================
+Performing exploratory Factor Analysis
+on the big five personalities dataset
+========================================
+"""
+import numpy as np
+import pandas as pd
+from sklearn import set_config
+
+from factor_analysis import FactorAnalysis
+
+# print all parameters of the estimator
+set_config(print_changed_only=False)
+
+# load and clean data
+df = pd.read_csv(r".\data\big_five_personality.csv", sep="\t")
+df = df[(df["IPC"] == 1)]  # cleaning repeaters (same IP)
+df.dropna(inplace=True)
+df.drop(df.columns[50:107], axis=1, inplace=True)
+df.drop(df.columns[51:], axis=1, inplace=True)
+df.drop("country", axis=1, inplace=True)
+
+# Fit the model
+fa = FactorAnalysis(n_factors=5, rotation="varimax", feature_names=df.columns)
+fa.fit(df)
+fa.summary()
+
+# test kmo
+print(
+    f"KMO for all variables > 0.6?: {np.all(FactorAnalysis.calculate_kmo(df)[0] > 0.6)}"
+)
