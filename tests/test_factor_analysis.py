6b58282cae6a9315f9b5646c5aeacb0b0a852381
Moritz Mistol <moritz.mistol@gmail.com>
2022-01-05 18:23:30 +0100

    update tests

--- tests/test_factor_analysis.py
+++ tests/test_factor_analysis.py
@@ -4,23 +4,8 @@ import numpy as np
 import pandas as pd
 import pytest
 from numpy.testing import assert_allclose
-from sklearn.datasets import load_iris
-from sklearn.utils.estimator_checks import check_estimator
 
 from factor_analysis import FactorAnalysis
-
-
-@pytest.mark.skip
-def test_estimator_check():
-    return check_estimator(FactorAnalysis())
-
-
-@pytest.mark.parametrize("n_factors", [2, 3])
-@pytest.mark.skip
-def test_iris(n_factors):
-    X = load_iris(as_frame=True).data
-    fa = FactorAnalysis(n_factors=n_factors).fit(X)
-    fa.print_summary()
 
 
 def test_book_example_two_factors(get_app_ex_data, get_app_ex_loadings):
@@ -29,24 +14,15 @@ def test_book_example_two_factors(get_app_ex_data, get_app_ex_loadings):
     loadings = get_app_ex_loadings
     communalities = [0.968, 0.526, 0.953, 0.991, 0.981]
     fa = FactorAnalysis(n_factors=2, is_corr_mtx=True).fit(corr)
+    assert fa.loadings_.shape == (data.shape[1], 2)
+
     assert_allclose(fa.loadings_, loadings, atol=1e-2)
     assert_allclose(fa.communalities_, communalities, atol=1e-2)
 
 
-@pytest.mark.parametrize("rotation", ["varimax", "oblimax"])
-def test_women_dataset(rotation):
-    pth = os.path.join(os.getcwd(), "data", "women_track_records.csv")
-    df = pd.read_csv(pth)
-
-    # first column is the country, so we drop it
-    df.drop(columns=["COUNTRY"], inplace=True)
-    fa = FactorAnalysis(n_factors=2, rotation=rotation).fit(df)
-    fa.print_summary()
-    # TODO: assert something
-
-
 def test_fit_using_corr_mtx(get_app_ex_loadings):
     loadings = get_app_ex_loadings
+    # Table 7.3, p. 421 Backhaus Multivariate Analysis (2021)
     R = np.array(
         [
             [1, 0.712, 0.961, 0.109, 0.044],
@@ -64,37 +40,9 @@ def test_fit_using_corr_mtx(get_app_ex_loadings):
     assert_allclose(fa.loadings_, loadings, atol=1e-2)
 
 
-def test_optimal_corr_mtx():
-    R = [
-        [1, 1, 0, 0, 0.05],
-        [1, 1, 0, 0.05, 0.05],
-        [0.05, 0.05, 1, 1, 0.9],
-        [0.05, 0, 1, 1, 0.9],
-        [0, 0.05, 0.9, 0.9, 1],
-    ]
-    fa = FactorAnalysis(
-        n_factors=2, is_corr_mtx=True, method="paf", initial_comm="ones"
-    ).fit(R)
-    fa.print_summary()
-
-
-@pytest.mark.parametrize("n_factors", [1, 2, 3, 4])
-@pytest.mark.skip("no assertions")
-def test_coastal_waves(n_factors):
-    pth = os.path.join(os.getcwd(), "data", "coastal_waves_data.csv")
-    df = pd.read_csv(pth, sep=",")
-    df.replace(-99.90, np.nan, inplace=True)
-    df.drop("Date/Time", axis=1, inplace=True)
-    df.dropna(inplace=True)
-    df.reset_index(drop=True, inplace=True)
-
-    # fa = FactorAnalysis(n_factors=n_factors).fit(df)
-    # fa.summary()
-
-
 @pytest.fixture(scope="session")
 def get_app_ex_data():
-    # Table 7.2 on p.387, Backhaus Multivariate Analysis
+    # Table 7.3 on p.419, Backhaus Multivariate Analysis
     pth = os.path.join(os.getcwd(), "data", "application_example_backhaus_2021.csv")
     data = pd.read_csv(pth, sep=";", header=None)
     return data
